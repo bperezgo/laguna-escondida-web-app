83a498e552e5a87300d83c69410778edcd26389a
bperezgo <bsperezgo@gmail.com>
2026-04-11 19:38:58 -0500

    feat: new version of laguna escondida web app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://lagunaescondida.com',
-  output: 'server',
-
+  site: 'https://lagunaescondida.com.co',
+  integrations: [sitemap()],
+  compressHTML: true,
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react(), sitemap()],
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
--- dist/server/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/node_COvPCf5W.mjs';
-export { renderers } from '../renderers.mjs';
