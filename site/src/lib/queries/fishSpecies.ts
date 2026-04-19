6c1a51ebb1b1cbc8ae81d3f8fcba7118cb899295
bperezgo <bsperezgo@gmail.com>
2026-04-19 15:46:43 -0500

    feat: new design fish species

--- site/src/lib/queries/fishSpecies.ts
+++ site/src/lib/queries/fishSpecies.ts
@@ -1,6 +1,11 @@
 export const FISH_SPECIES_QUERY = `*[_type == "fishSpecies"] | order(name asc) {
   name,
+  latinName,
   difficulty,
   description,
+  weightRange,
+  season,
+  depth,
+  bait,
   image
 }`;
